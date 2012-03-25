016c23915e9436f15c87c9ca715a0d40d7b380c1
tonyduke <chongyangdu@msn.com>
2012-03-25 19:14:28 +0800

    1

---
+++ phpstarter/CopyOfphpinfo.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title> php info - By:Do Anthony </title>
+</head>
+ 
+<body>
+<?php
+echo "<a href='http://localhost/~anthony/'>http://localhost/~anthony/index.html</a> <hr>";
+ 
+phpinfo();
+?>
+ 
+</body>
+</html>
